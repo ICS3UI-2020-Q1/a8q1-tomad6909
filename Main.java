bd189bece6bc6b884ecc21438e250d70ded050c8
tomad6909 <tomad6909@wrdsb.ca>
2020-11-02 03:44:18 +0000

    finished A8Q1

--- Main.java
+++ Main.java
@@ -5,29 +5,66 @@ import java.awt.event.*;
 public class Main implements Runnable, ActionListener{
 
   // Class Variables  
-  
+  JPanel mainPanel;
+
+  JLabel firstLabel;
+  JTextField firstInput;
+  JButton helloButton;
 
 
   // Method to assemble our GUI
   public void run(){
     // Creats a JFrame that is 800 pixels by 600 pixels, and closes when you click on the X
-    JFrame frame = new JFrame("Title");
+    JFrame frame = new JFrame("Hello");
     // Makes the X button close the program
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     // makes the windows 800 pixel wide by 600 pixels tall
-    frame.setSize(800,600);
+    frame.setSize(1000,800);
     // shows the window
     frame.setVisible(true);
+
+    mainPanel = new JPanel();
+
+    mainPanel.setLayout(null);
+
+
+
+    firstLabel = new JLabel();
+
+    firstLabel.setBounds(330, 300, 200, 120);
+
+
+    firstInput = new JTextField();
+
+    firstInput.setBounds(330, 200, 300, 130);
+
+
+    helloButton = new JButton("Say Hello");
+
+    helloButton.setBounds(350, 400, 250, 120);
+
+    helloButton.addActionListener(this);
+
+
+    mainPanel.add(firstLabel);
+    mainPanel.add(firstInput);
+    mainPanel.add(helloButton);
  
     
-
+    frame.add(mainPanel);
   }
 
   // method called when a button is pressed
   public void actionPerformed(ActionEvent e){
     // get the command from the action
     String command = e.getActionCommand();
+    if(command.equals("Say Hello")){
+      String firstText = firstInput.getText();
 
+      System.out.println(firstLabel + firstText);
+
+      firstLabel.setText("Hello" + firstText);
+    }
   }
 
   // Main method to start our program
